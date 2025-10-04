bbb6e2e7c9fef9760adbe37248a4ee61e2bc0039
DEEPAK <deepakofficial0103@gmail.com>
2025-10-04 15:07:46 +0530

    asd

--- frontend/website/src/App.jsx
+++ frontend/website/src/App.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./features/LandingPage";
 import Login from "./features/login";
-import LearnerDashboard from "./components/LearnerDashboard";
-import EmployerDashboard from "./components/EmployerDashboard";
-import InstitutionDashboard from "./components/InstitutionDashboard";
+//import LearnerDashboard from "./components/LearnerDashboard";
+//import EmployerDashboard from "./components/EmployerDashboard";
+//import InstitutionDashboard from "./components/InstitutionDashboard";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,10 +25,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={isLoggedIn ? <Navigate to={`/dashboard/${userRole.toLowerCase()}`} /> : <Login onAuthSuccess={handleLoginSuccess} />} />
-        <Route path="/dashboard/learner" element={isLoggedIn && userRole === "Learner" ? <LearnerDashboard onLogout={handleLogout} /> : <Navigate to="/login" />} />
+        {/* <Route path="/dashboard/learner" element={isLoggedIn && userRole === "Learner" ? <LearnerDashboard onLogout={handleLogout} /> : <Navigate to="/login" />} />
         <Route path="/dashboard/employer" element={isLoggedIn && userRole === "Employer" ? <EmployerDashboard onLogout={handleLogout} /> : <Navigate to="/login" />} />
         <Route path="/dashboard/institution" element={isLoggedIn && userRole === "Institution" ? <InstitutionDashboard onLogout={handleLogout} /> : <Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" />} /> */}
       </Routes>
     </Router>
   );
